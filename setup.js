6719d1a6cf494182d46b52d2681e66d4985fe7e1
thomkeh <thomas5max@gmail.com>
2021-04-09 15:53:07 +0100

    New style (#19)
    * Begin work on new style
    
    * Make prettier and add more content
    
    * Figure out how to get a dark background
    
    * Finish tweaking the new style for now

---
+++ setup.js
@@ -0,0 +1,42 @@
+import Reveal from './dist/reveal.esm.js';
+import Math from './plugin/math/math.esm.js';
+
+Reveal.initialize({
+  controls: false,
+  progress: false,
+  slideNumber: 'c/t',
+  center: false,
+  history: true,
+  transition: 'none',
+  width: 1120, // default 960
+  height: 700,
+  margin: 0.04,
+
+  math: {
+    // mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js',
+    config: 'TeX-AMS_HTML-full',
+    TeX: {
+      Macros: {
+        R: '\\mathbb{R}',
+        set: [ '\\left\\{#1 \\; ; \\; #2\\right\\}', 2 ]
+      }
+    }
+  },
+
+  plugins: [ Math ]
+});
+
+// make footer visible in normal mode and in print mode
+var footer = document.querySelector('#background-template');
+if (window.location.search.match( /print-pdf/gi )) {
+  Reveal.addEventListener('ready', function(event) {
+    // add footer to every slide
+    for (let slide of document.querySelectorAll('.slide-background')) {
+      if (slide.classList.contains('stack')) {
+        continue;
+      }
+      let clone = footer.cloneNode(true);
+      slide.appendChild(clone)
+    }
+  });
+}
